dd9d5f81b7f3cd4777ee1aec13915360c8393824
metamn <bartus.csongor@gmail.com>
2018-10-20 11:48:46 +0300

    Pulse updated

--- pulse/assets/scripts/project/spacer/spacer.js
+++
@@ -1,5 +0,0 @@
-var splitStringToChars = require('./../../framework/helpers/js/splitStringToChars.js');
-
-if (document.querySelector('.spacer')) {
-  splitStringToChars('.spacer__text', '.spacer__chars');
-}
