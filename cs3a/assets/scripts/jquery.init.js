606a475c0c2e50aac8d26f10076049e29d4a9759
metamn <bartus.csongor@gmail.com>
2019-04-30 14:07:13 +0300

    Adding cs3b

---
+++ cs3a/assets/scripts/jquery.init.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  
+  // Tooltip 
+  $('.tooltip-item').on('click', function() {
+    $('.tooltip span').html($(this).find('.tooltip-text').html());
+    $('.tooltip').addClass('active');
+    
+    $(this).parent().siblings().removeClass('active');
+    $(this).parent().addClass('active');
+  });
+  
+  $('.tooltip-item').first().trigger('click');
+  
+
+  // Click to show the menu
+  $('header').click(function() {
+    $('header').toggleClass('active');
+  });
+  
+});
