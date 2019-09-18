bd6288df23c7857c40a87f8bc64ae595b7430b21
metamn <bartus.csongor@gmail.com>
2019-09-18 19:01:21 +0300

    Deploying variable

--- variable-docs/service-worker.js
+++ variable-docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.d01a6b4cad793ce79100097c0b7a2b91.js"
+  "precache-manifest.ad44ca1db4e39ce75ab2c28dfaa56816.js"
 );
 
 self.addEventListener('message', (event) => {
