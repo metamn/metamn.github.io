1b595507147432d78cede4c9bfd2a581f08bf14b
metamn <bartus.csongor@gmail.com>
2020-01-25 14:00:00 +0200

    Updating Pulse

---
+++ pulse/assets/scripts/pages/home/home.js
@@ -0,0 +1,1 @@
+require('./../../project/spacer/spacer.js');
---
+++ pulse/assets/scripts/project/spacer/spacer.js
@@ -0,0 +1,5 @@
+var splitStringToChars = require('./../../framework/helpers/js/splitStringToChars.js');
+
+if (document.querySelector('.spacer')) {
+  splitStringToChars('.spacer__text', '.spacer__chars');
+}
