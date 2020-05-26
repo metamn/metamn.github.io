6fd37119bdc3c89d63e984952506ba6665207ffb
metamn <bartus.csongor@gmail.com>
2020-05-26 08:20:20 +0300

    Deploying Finsterdata

---
+++ finsterdata/precache-manifest.8f651a23a05895461c99f2fc268046e5.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23af05a20f151af49ab9a0ec903af62c",
+    "url": "/finsterdata/index.html"
+  },
+  {
+    "revision": "055122ac27f86f0c90f3",
+    "url": "/finsterdata/static/js/2.938d684a.chunk.js"
+  },
+  {
+    "revision": "a53777903325fa5744582cb16cbd1783",
+    "url": "/finsterdata/static/js/2.938d684a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "461cf1b3dfdf2405d740",
+    "url": "/finsterdata/static/js/main.59db5e2e.chunk.js"
+  },
+  {
+    "revision": "609fdcc3648c72ed6267",
+    "url": "/finsterdata/static/js/runtime-main.5530659c.js"
+  },
+  {
+    "revision": "c663e76e0e6cb6117b50b7a9f6c2c7af",
+    "url": "/finsterdata/static/media/chat-1.c663e76e.svg"
+  },
+  {
+    "revision": "4fc602a1b93a36b9da1242c9ef342046",
+    "url": "/finsterdata/static/media/contact-bg.4fc602a1.png"
+  },
+  {
+    "revision": "b67a62822ffba51291828db4ca3ffe45",
+    "url": "/finsterdata/static/media/facebook.b67a6282.svg"
+  },
+  {
+    "revision": "cb692f4add12486e42a71d0306683f3f",
+    "url": "/finsterdata/static/media/factory.cb692f4a.svg"
+  },
+  {
+    "revision": "44584b4c10e7a9238876356f82389c50",
+    "url": "/finsterdata/static/media/feature-banner-1.44584b4c.svg"
+  },
+  {
+    "revision": "ec0f446172cc63b57200c9af62b71e1c",
+    "url": "/finsterdata/static/media/feature-banner-2.ec0f4461.svg"
+  },
+  {
+    "revision": "685aa8bb2cb1bbd7d2cfeb394fc500a8",
+    "url": "/finsterdata/static/media/features-bg-2.685aa8bb.png"
+  },
+  {
+    "revision": "10acd1cc2606eede3acfe53a66b3b63e",
+    "url": "/finsterdata/static/media/features-bg.10acd1cc.png"
+  },
+  {
+    "revision": "d57e776ca3640a704ad8dddf731cca96",
+    "url": "/finsterdata/static/media/footer.d57e776c.png"
+  },
+  {
+    "revision": "da40935af9b12178c1478905848356c2",
+    "url": "/finsterdata/static/media/google.da40935a.svg"
+  },
+  {
+    "revision": "b38a7524a7aac81e306293de5516e70d",
+    "url": "/finsterdata/static/media/instagram.b38a7524.svg"
+  },
+  {
+    "revision": "19c213f3738734f633b573d697ff2e31",
+    "url": "/finsterdata/static/media/laptop-group-left.19c213f3.png"
+  },
+  {
+    "revision": "70c3f6ef4d370b157db1e1a31e4937c3",
+    "url": "/finsterdata/static/media/laptop-group-right.70c3f6ef.png"
+  },
+  {
+    "revision": "0c5d8fc2422221e045c18b8a3241ff5f",
+    "url": "/finsterdata/static/media/lines.0c5d8fc2.png"
+  },
+  {
+    "revision": "c8fee8b961eec8a44ec21c545747dcdc",
+    "url": "/finsterdata/static/media/linkedin.c8fee8b9.svg"
+  },
+  {
+    "revision": "525a815b841783bfed51b0748f5115a1",
+    "url": "/finsterdata/static/media/logo.525a815b.svg"
+  },
+  {
+    "revision": "6104426d059901458337548eb8a45e64",
+    "url": "/finsterdata/static/media/plans-bg.6104426d.png"
+  },
+  {
+    "revision": "01ea0c12887b04170545406d163eaac5",
+    "url": "/finsterdata/static/media/printer.01ea0c12.svg"
+  },
+  {
+    "revision": "831e1ce176b88bcd3ac177b90b8df3ab",
+    "url": "/finsterdata/static/media/settings-1.831e1ce1.svg"
+  },
+  {
+    "revision": "b48162696cfa49245472b1c0f20e1f7a",
+    "url": "/finsterdata/static/media/upload.b4816269.svg"
+  },
+  {
+    "revision": "9b14728ffd11f40e034e7c39c2ccfaae",
+    "url": "/finsterdata/static/media/website.9b14728f.svg"
+  }
+]);
