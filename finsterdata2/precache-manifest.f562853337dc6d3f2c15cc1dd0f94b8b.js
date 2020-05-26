eeb15d49cb0652864f02ccf3a8ffec0a8d5a511b
metamn <bartus.csongor@gmail.com>
2020-05-26 08:04:53 +0300

    Adding Finsterdata 2

---
+++ finsterdata2/precache-manifest.f562853337dc6d3f2c15cc1dd0f94b8b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bfe7d74086e82688abdc2093e6470fc",
+    "url": "/finsterdata2/index.html"
+  },
+  {
+    "revision": "e1e381e1f7f187a5c0e5",
+    "url": "/finsterdata2/static/js/2.bd9bd8be.chunk.js"
+  },
+  {
+    "revision": "46cccfb168a1755be4eef51611b873d4",
+    "url": "/finsterdata2/static/js/2.bd9bd8be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b30084359d6541620f60",
+    "url": "/finsterdata2/static/js/main.e8a5b1f3.chunk.js"
+  },
+  {
+    "revision": "71f481605549fafc9bf9",
+    "url": "/finsterdata2/static/js/runtime-main.16805621.js"
+  },
+  {
+    "revision": "b0d16765e363482eb2f9ddcac52c552b",
+    "url": "/finsterdata2/static/media/2ndFimage.b0d16765.png"
+  },
+  {
+    "revision": "1aee30800b625b47cfb7e0fa639c4ded",
+    "url": "/finsterdata2/static/media/Group-21295.1aee3080.svg"
+  },
+  {
+    "revision": "44584b4c10e7a9238876356f82389c50",
+    "url": "/finsterdata2/static/media/Group21249.44584b4c.svg"
+  },
+  {
+    "revision": "8b1f57d7ebf7d9bcbc9441dec91b93a9",
+    "url": "/finsterdata2/static/media/Logo.8b1f57d7.svg"
+  },
+  {
+    "revision": "1ccdfe27e23963bba4fc868f0d32e2f1",
+    "url": "/finsterdata2/static/media/bottom-section.1ccdfe27.png"
+  },
+  {
+    "revision": "c951b9dde2bb69e55300bf3a8d282142",
+    "url": "/finsterdata2/static/media/dots.c951b9dd.png"
+  },
+  {
+    "revision": "f06e7953fc7bb69fbb016ebc5a5adf1f",
+    "url": "/finsterdata2/static/media/footer-orange-bg.f06e7953.svg"
+  },
+  {
+    "revision": "82111a84bc90991841f6cecea8be3d43",
+    "url": "/finsterdata2/static/media/group.82111a84.png"
+  },
+  {
+    "revision": "e8f4d1d47623b5979a2ee5208bc3bd9f",
+    "url": "/finsterdata2/static/media/orange-banner.e8f4d1d4.png"
+  }
+]);
